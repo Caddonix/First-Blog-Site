5b50a4e6bc0086f2fa1fec1d5dab24fa36db455b
Purva_Anjarlekar <Purva_Anjarlekar@users.noreply.github.com>
2020-06-23 20:37:07 +0530

    The first upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Purva Anjarlekar',
+        'title': 'Blog Post 1',
+        'content': 'First post ever',
+        'date_posted': '27th November 2019'
+    },
+
+    {
+        'author': 'Abhay Ubhale',
+        'title': 'Blog Post 2',
+        'content': 'Second post',
+        'date_posted': '29th November 2019'
+    },
+]
+
+@app.route('/')
+def home():
+    return render_template('home.html', posts= posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
